29db0c2867860ecef568786d8182c7462ad2b8d0
Lyncon Estevan Bernardo Baez <lynconlyn@gmail.com>
2020-12-23 13:49:59 -0300

    Fix errors

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const createVertices = () => {
-    const triangulo = new Float32Array([-1,1,0,0.5,1,0,-1,1,0]);
+    const triangulo = new Float32Array([
+        -1,  -1, 0,
+        0., 1, 0, 
+        1,  -1, 0
+    ]);
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER,triangulo,gl.STATIC_DRAW);
-    gl.vertexAttribPointer(0,3,gl.FLOAT,false,12,0);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 12, 0);
     gl.enableVertexAttribArray(0);
     return vertexBuffer;
 }
@@ -15,13 +19,13 @@ const createVertexShader = (program) => {
         layout (location = 0) in vec3 aPos;
     
         void main() {
-            gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);
+            gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
         }
     `;
     const vertexShaderBuffer = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShaderBuffer,vertexShader);
+    gl.shaderSource(vertexShaderBuffer, vertexShader);
     gl.compileShader(vertexShaderBuffer);
-    //console.log(gl.getShaderInfoLog(vertexShaderBuffer));
+    console.log(gl.getShaderInfoLog(vertexShaderBuffer));
     return vertexShaderBuffer;
 }
 
@@ -29,7 +33,7 @@ const createFragmentShader = (program) => {
     const fragmentShader = `#version 300 es
         precision highp float;
         out vec4 FragColor;
-        const vec4 cor = vec4(1.0,0.,0.,1.);
+        const vec4 cor = vec4(1.0, 0., 0., 1.);
         void main() {
             FragColor = cor;
         }
@@ -37,7 +41,7 @@ const createFragmentShader = (program) => {
     const fragmentShaderBuffer = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShaderBuffer,fragmentShader);
     gl.compileShader(fragmentShaderBuffer);
-    //console.log(gl.getShaderInfoLog(fragmentShaderBuffer));
+    console.log(gl.getShaderInfoLog(fragmentShaderBuffer));
     return fragmentShaderBuffer;
 }
 
@@ -45,13 +49,14 @@ const canvas = document.getElementById("canvas");
 const gl = canvas.getContext("webgl2");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-gl.clearColor(0,0,1,1);
+gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
 const program = gl.createProgram();
-const vertexShaderBuffer = createFragmentShader(program);
-const fragmentShaderBuffer = createVertexShader(program);
+const fragmentShaderBuffer= createFragmentShader(program);
+const vertexShaderBuffer = createVertexShader(program);
 const vertexBuffer = createVertices();
+
 
 gl.attachShader(program,fragmentShaderBuffer);
 gl.attachShader(program,vertexShaderBuffer);
@@ -59,7 +64,7 @@ gl.linkProgram(program);
 gl.useProgram(program);
 gl.deleteShader(fragmentShaderBuffer);
 gl.deleteShader(vertexShaderBuffer);
-
+console.log(gl.getProgramInfoLog(program));
 
 gl.drawArrays(gl.TRIANGLES,0,3);
 
